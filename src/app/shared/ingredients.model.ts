0a579c20ff567b7638cc4acb5b958ffb75c6f955
Elena Arocena <elena.arocena@innoway.es>
2023-09-07 17:00:18 +0200

    feat(shopping): add models for recipes and create structure for shopping zone

---
+++ src/app/shared/ingredients.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+  public name: string;
+  public amount: number;
+  public imagePath: string;
+
+  constructor(name: string, amount: number){
+    this.name = name;
+    this.amount = amount;
+  }
+
+
+}
